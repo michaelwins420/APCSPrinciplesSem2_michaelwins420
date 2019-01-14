92ebcf9ef4f9d1f0b161092881abead1def3c6a3
michaelwins420 <43476753+michaelwins420@users.noreply.github.com>
2019-01-14 08:50:30 -0800

    Add files via upload

---
+++ Lab BubbleSort/sketch.js
@@ -0,0 +1,36 @@
+var i = [3,5,1,2,4,7.6];
+var j = [3,5,1,2,4,7.6];
+
+
+//  This is a comment
+//  The setup function function is called once when your program begins
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+  console.log(a);
+  mySort();
+  console.log(b);
+  bubbleSort();
+
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+
+}
+
+
+
+
+function bubbleSort(){
+   for (var i = len-1; i>=0; i--){
+     for(var j = 1; j<=i; j++){
+       if(arr[j-1]>arr[j]){
+           var temp = arr[j-1];
+           arr[j-1] = arr[j];
+           arr[j] = temp;
+        }
+     }
+   }
